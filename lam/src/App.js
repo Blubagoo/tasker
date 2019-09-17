de64d705db75710e7600bafc1a552fab63b05423
Jim Tisdale <JTisdale@LAM.local>
2019-09-17 08:51:44 -0500

    initial commit

---
+++ lam/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Shell from './components/shell';
+
+function App() {
+  return (
+    <div className="App">
+      <Shell />
+    </div>
+  );
+}
+
+export default App;
---
+++ lam/src/components/shell.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Shell extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div className="shell">
+				<p>Hello World</p>
+			</div>
+		)
+	}
+}
